0a07d069004388bc62dc59c2aeb17552c2564c3e
Isaac Jones <isj@trustpilot.com>
2026-02-24 11:21:37 +0000

    Updates

--- scripts/script.py
+++ scripts/script.py
@@ -5,4 +5,5 @@ services = ["api", "worker", "scheduler"]
 
 # GitHub Actions step output
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-    f.write(f"matrix={json.dumps(services)}\n")
+    # f.write(f"matrix={json.dumps(services)}\n")
+    f.write(f"matrix={services}")
